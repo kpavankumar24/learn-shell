e9283a4ddd97bb103840a194479ceefe7b0cd8f9
PAVAN KUMAR <kpavankumar2410@gmail.com>
2023-11-10 08:49:30 -0500

    Making shell scripting topics-add underline

---
+++ 02-print.sh
@@ -0,0 +1,11 @@
+# echo -e "\e[31m message \e[0m"
+# there are total six colors each color has different code
+# red -31
+# green -32
+# yellow -33
+#blue -34
+#magenta -35
+#cyan-36
+
+echo -e "\e[35m this is magenta color \e[0m"
+echo -e "\e[31m this code is used to print the message in red \e[0m"
